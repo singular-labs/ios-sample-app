480d82614896bee38e62b9ec185575650fcfbff3
Jared Ornstead <jared@singular.net>
2021-11-18 16:34:31 -0800

    Updated Custom Event Controller with Event Length Validation

--- ios-sample-app/ios-sample-app/Controllers/CustomEventsController.m
+++ ios-sample-app/ios-sample-app/Controllers/CustomEventsController.m
@@ -13,43 +13,56 @@
 @implementation CustomEventsController
 
 - (IBAction)sendEvent:(id)sender {
-    NSString* eventName =self.eventNameField.text;
+    NSString* eventName = self.eventNameField.text;
     
     if([Utils isEmptyOrNull:eventName]){
-        [Utils displayMessage:@"Please enter a valid event name" withView:self];
-        return;
+        [Utils displayMessage:@"Please enter a valid event name." withView:self];
+    } else {
+        if(eventName.length <= 32){
+            //Add code here
+            // Set Conversion Value manually (when using manualSkanConversionManagement)
+            // Note that conversion values may only increase, so only the first call will update it
+            [Singular skanUpdateConversionValue:7];
+            
+            // Reporting a simple event to Singular
+            [Singular event:eventName];
+            
+            NSLog(@"Event Sent: %@", eventName);
+            [Utils displayMessage:@"Event sent" withView:self];
+            
+        } else {
+            [Utils displayMessage:@"Event name is too long. Event name length limit is 32 characters." withView:self];
+        }
     }
-    
-    // Set Conversion Value manually (when using manualSkanConversionManagement)
-    // Note that conversion values may only increase, so only the first call will update it
-    [Singular skanUpdateConversionValue:7];
-    
-    // Reporting a simple event to Singular
-    [Singular event:eventName];
-    
-    [Utils displayMessage:@"Event sent" withView:self];
+    return;
 }
 
 - (IBAction)sendEventsWithAttributes:(id)sender {
-    NSString* eventName =self.eventNameField.text;
+    NSString* eventName = self.eventNameField.text;
     
-     if([Utils isEmptyOrNull:eventName]){
-         [Utils displayMessage:@"Please enter a valid event name" withView:self];
-        return;
+    if([Utils isEmptyOrNull:eventName]){
+        [Utils displayMessage:@"Please enter a valid event name." withView:self];
+    } else {
+        if(eventName.length <= 32){
+            NSMutableDictionary* args = [[NSMutableDictionary alloc] init];
+            [args setObject:@"value1" forKey:@"key1"];
+            [args setObject:@"value2" forKey:@"key2"];
+            
+            // Set Conversion Value manually (when using manualSkanConversionManagement)
+            // Note that conversion values may only increase, so only the first call will update it
+            [Singular skanUpdateConversionValue:3];
+            
+            // Reporting a simple event with your custom attributes to pass with the event
+            [Singular event:eventName withArgs:args];
+            
+            NSLog(@"Event Sent with Args: %@", eventName);
+            [Utils displayMessage:@"Event sent" withView:self];
+            
+        } else {
+            [Utils displayMessage:@"Event name is too long. Event name length limit is 32 characters." withView:self];
+        }
     }
-    
-    NSMutableDictionary* args = [[NSMutableDictionary alloc] init];
-    [args setObject:@"value1" forKey:@"key1"];
-    [args setObject:@"value2" forKey:@"key2"];
-    
-    // Set Conversion Value manually (when using manualSkanConversionManagement)
-    // Note that conversion values may only increase, so only the first call will update it
-    [Singular skanUpdateConversionValue:3];
-    
-    // Reporting a simple event with your custom attributes to pass with the event
-    [Singular event:eventName withArgs:args];
-    
-    [Utils displayMessage:@"Event sent" withView:self];
+    return;
 }
 
 @end
